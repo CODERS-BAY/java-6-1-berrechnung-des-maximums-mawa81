085a9c1cfcdb1d179dc28570b11b7194d002c318
Mario Wagner <ma_wa@gmx.at>
2020-02-10 15:10:10 +0100

    Created new Package and Class

--- src/com/codersbay/arraysmario/Aufgabe6_1_Maxima_Calculation.java
+++ src/com/codersbay/arraysmario/Aufgabe6_1_Maxima_Calculation.java
@@ -1,7 +1,29 @@
 package com.codersbay.arraysmario;
 
+import java.util.Scanner;
+
 public class Aufgabe6_1_Maxima_Calculation {
     public static void main(String[] args) {
+        int[] list = new int[0];
+        //String lst = "";
+        Scanner scan = new Scanner(System.in);
+        int number;
+        int count = 0;
+        while (!scan.hasNext("q")) {
+            number = scan.nextInt();
+            //System.out.println(number);
+            // lst+=number;
 
+            int[] tmp = new int[list.length + 1];
+            for (int i = 0; i < list.length; i++) {
+                tmp[i] = list[i];
+            }
+            tmp[count] = number;
+            list = tmp;
+            count++;
+        }
+        for (int j : list) {
+            System.out.println(j);
+        }
     }
 }
