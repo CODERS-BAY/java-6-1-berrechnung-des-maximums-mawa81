a25a650bd59063647574b541957884f1ff6d885e
Mario Wagner <ma_wa@gmx.at>
2020-02-12 10:38:18 +0100

    Added input-text, error handling and the maximum value detection

--- src/com/codersbay/Main.java
+++
@@ -1,5 +0,0 @@
-package com.codersbay;
-
-public class Main {
-
-}
--- src/com/codersbay/arraysmario/Aufgabe6_1_Maxima_Calculation.java
+++ src/com/codersbay/arraysmario/Aufgabe6_1_Maxima_Calculation.java
@@ -5,25 +5,33 @@ import java.util.Scanner;
 public class Aufgabe6_1_Maxima_Calculation {
     public static void main(String[] args) {
         int[] list = new int[0];
-        //String lst = "";
         Scanner scan = new Scanner(System.in);
-        int number;
+        int number = 0;
         int count = 0;
+        System.out.println("Please enter some numbers (q for quit)");
         while (!scan.hasNext("q")) {
-            number = scan.nextInt();
-            //System.out.println(number);
-            // lst+=number;
-
-            int[] tmp = new int[list.length + 1];
-            for (int i = 0; i < list.length; i++) {
-                tmp[i] = list[i];
+            if (scan.hasNextInt()) {
+                number = scan.nextInt();
+                int[] tmp = new int[list.length + 1];
+                for (int i = 0; i < list.length; i++) {
+                    tmp[i] = list[i];
+                }
+                tmp[count] = number;
+                list = tmp;
+                count++;
+            } else {
+                scan.next();
+                System.out.println("Input not possible!");
+                System.out.println("Please enter numbers (q for quit)");
             }
-            tmp[count] = number;
-            list = tmp;
-            count++;
         }
+        int max_number = number;
         for (int j : list) {
             System.out.println(j);
+            if (max_number < j) {
+                max_number = j;
+            }
         }
+        System.out.println("The maximum value is " + max_number + ".");
     }
 }
